f2259e09ebe53abba906a7b4422e3ffefc00ed46
Salma Anany <salmayousry5@gmail.com>
2024-09-23 14:34:02 -0700

    funcation2

--- function_b.py
+++ function_b.py
@@ -2,7 +2,7 @@
 # Iterating Over Data
 # Problem-Set While Loops #11
 def silly_sum():
-    """ reads numbers from the user (use input_int) 
+    """ reads numbers from the user
         summing as we go until either
         the user enters 0, or
         the sum reaches or exceeds 1000
